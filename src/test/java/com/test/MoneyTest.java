b8749e106c4275ff6c82ccbaa19aed112d52226a
mohamed-aly <mali902016@gmail.com>
2020-09-13 15:26:01 +0200

    removed subclasses

--- src/test/java/com/test/MoneyTest.java
+++ src/test/java/com/test/MoneyTest.java
@@ -8,40 +8,32 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money dollar = Money.dollar(5);
+        assertEquals(Money.dollar(10), dollar.times(2));
+        assertEquals(Money.dollar(15), dollar.times(3));
+
+        Money franc = Money.franc(5);
+        assertEquals(Money.franc(10), franc.times(2));
+        assertEquals(Money.franc(15), franc.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(8));
-    }
 
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         assertEquals(Money.franc(5), Money.franc(5));
         assertNotEquals(Money.franc(5), Money.franc(8));
     }
 
     @Test
-    void testCurrencyFranc() {
+    void testCurrency() {
         assertEquals("CHF", Money.franc(5).currency());
         assertNotEquals("CHF", Money.dollar(5).currency());
-    }
 
-    @Test
-    void testCurrencyDollar() {
         assertEquals("USD", Money.dollar(5).currency());
         assertNotEquals("USD", Money.franc(5).currency());
     }
+
 }
